d5d1c3513679370df235a5e6183094314bd7070b
Lesly Sarahi Peña Guerra <pleslysarahi@gmail.com>
2025-02-18 10:09:53 -0600

    orden de trabajo

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const dayjs = require("dayjs");
 const { formatearNumero } = require("./helpers/formato");
 // Conectar al servidor en la nube
 // const socket = io("http://localhost:3005"); // Cambia por la dirección IP de tu servidor
-// const socket = io("https://backendopticaecheverria-production-eeb4.up.railway.app"); // Echeverria Cambia por la dirección IP de tu servidor
-const socket = io("https://centrovisualpsicocristiano-production.up.railway.app"); // Centro visual cristiano
+const socket = io("https://backendopticaecheverria-production-eeb4.up.railway.app"); // Echeverria Cambia por la dirección IP de tu servidor
+// const socket = io("https://centrovisualpsicocristiano-production.up.railway.app"); // Centro visual cristiano
 // const socket = io("https://opticavisualhn-production.up.railway.app/"); // Optica visual HN Progreso
 // const socket = io("https://backendcentrooptico-production.up.railway.app/"); // Centro Optico cristiano
 
@@ -24,7 +24,7 @@ socket.on("printFactura", (data) => {
   let { datosImprimir } = data;
   // console.log(datosImprimir);
 
-  if (datosImprimir.sucursales !== "679bc095b62802c2d30aeb33") {
+  if (datosImprimir.sucursales !== "6783ce51f8cf2be5252615fc") {
     return;
   }
   try {
@@ -147,7 +147,7 @@ socket.on("printFactura", (data) => {
 
 socket.on("printRecibo", (data) => {
   let { datosImprimir } = data;
-  if (datosImprimir.sucursales !== "679bc095b62802c2d30aeb33") {
+  if (datosImprimir.sucursales !== "6783ce51f8cf2be5252615fc") {
     return;
   }
   try {
@@ -226,6 +226,100 @@ socket.on("printRecibo", (data) => {
           .text(datosImprimir.paginaDigital)
           .feed(3)
           // .beep(1, 100)
+          .cut()
+          .close();
+      //}
+    });
+  } catch (error) {
+    console.error("Error al imprimir:", error);
+  }
+});
+
+socket.on("printOrdenTrabajo", (data) => {
+  let { datosImprimir } = data;
+  if (datosImprimir.sucursalId !== "6783ce51f8cf2be5252615fc") {
+    return;
+  }
+  try {
+    const device = new escpos.USB();
+    const printer = new escpos.Printer(device);
+
+    const table = `
+    <table style='width:100%' class='receipt-table' border='0'>
+      <thead>
+        <tr class='heading'>
+          <th>Descripcion</th>
+        </tr>
+      </thead>
+        <tbody>
+          ${datosImprimir.articulos.map(
+      (item) =>
+        `
+                <tr>
+                  <td>${item.descripcion.toLocaleUpperCase()}</td>
+                 </tr>
+                `
+    )}
+        </tbody>
+    </table>
+  `;
+
+    const textHtml = htmlToText.convert(table, {
+      wordwrap: false,
+      tables: [".receipt-box", ".receipt-table"],
+    });
+
+    device.open(function (error) {
+      if (error) {
+        console.error("Error al abrir el dispositivo:", error);
+        return;
+      }
+     // for (let index = 0; index < 2; index++) {
+        printer
+          .font("a")
+          .align("LT")
+          .encode("utf8")
+          .size(0, 0)
+          .text(datosImprimir.nombreSucursal)
+          .size(0, 0)
+          .text(`Direccion: ${datosImprimir.direccion}`)
+          .text(`Telefono: ${datosImprimir.telefono}  Edad: ${datosImprimir.edad}`)
+          .text(`OD E: ${datosImprimir.recetaOjoDerecho.esfera}  C: ${datosImprimir.recetaOjoDerecho.cilindro} Eje: ${datosImprimir.recetaOjoDerecho.eje} Add: ${datosImprimir.recetaOjoDerecho.adicion} DP: ${datosImprimir.recetaOjoDerecho.distanciaPupilar}`)
+          .text(`OI E: ${datosImprimir.recetaOjoIzquierdo.esfera}  C: ${datosImprimir.recetaOjoIzquierdo.cilindro} Eje: ${datosImprimir.recetaOjoIzquierdo.eje} Add: ${datosImprimir.recetaOjoIzquierdo.adicion} DP: ${datosImprimir.recetaOjoIzquierdo.distanciaPupilar}`)
+          .text(textHtml)
+
+
+          .text(`Ticket # ${datosImprimir.numFacRec}`)
+          .text(`FECHA: ${dayjs().format("YYYY-MM-DD hh:mm a")}`)
+          .text(`Vendedor: ${datosImprimir.vendedor}`)
+          .text("")
+          .text(`Cliente: ${datosImprimir.cliente}`)
+          .text("")
+          .text(`Total: ${formatearNumero(datosImprimir.total)}`)
+          .text(`Acuenta: ${formatearNumero(datosImprimir.acuenta)}`)
+          .text(
+            `Resta : ${formatearNumero(
+              datosImprimir.total - datosImprimir.acuenta
+            )}`
+          )
+          .text("")
+          .text("Pago agregado")
+          .text(`Cantidad L ${formatearNumero(datosImprimir.monto)}`)
+          .text(`Fecha ${dayjs(datosImprimir.fecha).format("YYYY-MM-DD hh:mm a")}`)
+          .text(`Forma de pago ${datosImprimir.formaPago}`)
+          .feed(3)
+          .drawLine()
+          .text(datosImprimir.cliente.toLocaleUpperCase())
+          .text("Firma de autorizacion")
+          .text("")
+          .text("Recuerda que tu ticket es tu GARANTIA, consevalo")
+          .text(datosImprimir.mensajeFactura.toLocaleUpperCase())
+          .text("Nuestros datos de contacto:")
+          .text(datosImprimir.direccion)
+          .text(`TEL: ${datosImprimir.tel} / CEL: ${datosImprimir.cel}`)
+          .text(datosImprimir.email)
+          .text(datosImprimir.paginaDigital)
+          .feed(3)
           .cut()
           .close();
       //}
